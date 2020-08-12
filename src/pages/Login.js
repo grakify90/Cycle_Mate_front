9f24dc8623d111d3703db3dc5865878ee1a00411
grakify90 <ninavanes1990@hotmail.com>
2020-08-12 17:06:33 +0200

    made work and applied styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import { login } from "../store/user/actions";
 import { selectToken } from "../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
+import "./Login.scss";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
@@ -32,30 +33,35 @@ export default function SignUp() {
       <form>
         <h1>Login</h1>
         <div className="formContainer">
-          <label className="tripBar">Email address</label>
-          <input
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            type="email"
-            placeholder="Enter email"
-            required
-          />
+          <div className="formItemContainer">
+            <label className="tripBar">Email address</label>
+            <input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+          </div>
+
+          <div className="formItemContainer">
+            <label className="tripBar">Password</label>
+            <input
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </div>
         </div>
 
-        <div className="formContainer">
-          <label>Password</label>
-          <input
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            type="password"
-            placeholder="Password"
-            required
-          />
-        </div>
         <div>
           <button onClick={submitForm}>Log in</button>
         </div>
-        <Link to="/signup">Click here to sign up</Link>
+        <Link to="/signup" style={{ textDecoration: "none", color: "#c3aed6" }}>
+          Click here to sign up
+        </Link>
       </form>
     </div>
   );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { signUp } from "../store/user/actions";
-import { selectToken } from "../store/user/selectors";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory, Link } from "react-router-dom";
+// import { signUp } from "../store/user/actions";
+// import { selectToken } from "../store/user/selectors";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useHistory, Link } from "react-router-dom";
 
 export default function SignUp() {
   //   const [name, setName] = useState("");
